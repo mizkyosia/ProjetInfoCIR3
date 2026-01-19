d6944362988993ef563fdf107ab55d1838ec3691
MizKyosia <mizkyosia@gmail.com>
2026-01-19 20:27:47 +0100

    Preview test

---
+++ src/lib/components/PresentationPreview.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+    import { getImageURL } from "$lib/db";
+    import type { Presentation } from "$lib/types/presentation";
+
+    const { presentation }: { presentation: Presentation } = $props();
+
+    let src = $state("");
+    getImageURL(presentation.id + "-snapshot").then(s => src = s ?? "default.png");
+</script>
+
+<div class="hover:border-2 border-b-cyan-400 border-0 border-r-4 w-sm">
+    <img {src} alt="Snapshot de présentation">
+    <h3>{presentation.title}</h3>
+    <h4>{new Date(presentation.updatedAt).toDateString()}</h4>
+</div>
